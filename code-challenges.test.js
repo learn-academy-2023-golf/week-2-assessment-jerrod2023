13599bf40238b6df47c73723bd0108fab1527cba
Jerrod <chaddjed@gmail.com>
2023-10-15 21:06:58 -0700

    add week 2 assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -13,39 +13,100 @@
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const numbersArray1 = [6, 7, 8, 9, 10]
-// Expected output: [18, 21, 24, 27, 30]
-const numbersArray2 = [24, 27, 30, 33, 36]
-// Expected output: [72, 81, 90, 99, 108]
+// describe("numMultBy3", () => {
+//     it("takes in an array of numbers and returns an array with all the numbers multiplied by 3.", () => {
+//         const numbersArray1 = [6, 7, 8, 9, 10]
+//         const numbersArray2 = [24, 27, 30, 33, 36]
+//       //expect(numMultBy3r(numbersArray1)).toEqual([18, 21, 24, 27, 30])
+//       //expect(numMultBy3r(numbersArray1)).toEqual([72, 81, 90, 99, 108])
+//     })
+//   })
 
-// b) Create the function that makes the test pass.
+//   const numMultBy3 = (array) => { 
+//     return array.map(numbersArray1 => numbersArray1 * 3) 
+//     return array.map(numbersArray2 => numbersArray2 * 3)
+// }
 
-// Pseudo code:
+  
+// // b) Create the function that makes the test pass.
+
+
+// const numbersArray1 = [6, 7, 8, 9, 10]
+// const numbersArray2 = [24, 27, 30, 33, 36]
+// // Pseudo code:
+//     //Create a Function = "numMultBy3"
+//     ////.map method to return the new arrays    that will multiply by 3
+//     // //input: Arrays of [6, 7, 8, 9, 10] and [24, 27, 30, 33, 36]
+//     //output:
+// const numMultBy3 = (array) => { 
+//     return array.map(numbersArray1 => numbersArray1 * 3) 
+//     return array.map(numbersArray2 => numbersArray2 * 3)
+// }
+
+    
+
 
 // --------------------1) Create a function that takes a object as an argument and decides if the number inside it is evenly divisible by three or not.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const object1 = { number: 15 }
-// Expected output: "15 is divisible by three"
-const object2 = { number: 0 }
-// Expected output: "0 is divisible by three"
-const object3 = { number: -7 }
+// describe("numDivBy3", (number) => {
+//     it("takes a object as an argument and decides if the number inside it is evenly divisible by three or not.", () => {
+//       expect(numDivBy3()).toEqual("15 is divisible by three")
+//       expect(numDivBy3()).toEqual("0 is divisible by three")
+//       expect(numDivBy3()).toEqual("-7 is divisible by three")
+//     })
+//   })
+  
+// // 
+// const numDivBy3 = () => {
+//     if (number%3 === 0) {
+//         return "15 is divisible by three"
+//     } else if (number%3 === 1) {
+//         return "0 is divisible by three"
+//     } else if (number%3 === -1) {
+//         return "-7 is divisible by three"
+//     } else {
+//         return "error"
+//     }
+// }
+
+// const object1 = { number: 15 }
+// // Expected output: "15 is divisible by three"
+// const object2 = { number: 0 }
+// // Expected output: "0 is divisible by three"
+// const object3 = { number: -7 }
 // Expected output: "-7 is not divisible by three"
 
 // b) Create the function that makes the test pass.
 
 // Pseudo code:
+//              Create a function = numDivBy3
+            //Parameter = number
+            //create a conditional statement
+            
 
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
 
 // a) Create a test with expect statements for each of the variables provided.
-
-const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
-// Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
-const randomNouns2 = ["temperature", "database", "chopsticks", "mango"]
+// describe("newArray", () => {
+//     it("takes in an array of words and returns an array with all the words capitalized.", () => {
+//       expect(randomNouns1()).toEqual["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
+//       expect(randomNouns2()).toEqual["Temperature", "Database", "Chopsticks", "Mango"]
+//     })
+//   })
+// const newArray = (array, string) => {
+//     return array.toUpperCase[0](randomNouns1)
+//     return array.toUpperCase[0](randomNouns2)
+// }
+// const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
+// // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
+// const randomNouns2 = ["temperature", "database", "chopsticks", "mango"]
 // Expected output: ["Temperature", "Database", "Chopsticks", "Mango"]
 
 // b) Create the function that makes the test pass.
 
 // Pseudo code:
+    //create a function = newArray
+    //parameter (arrat, string)
+    //use .toUpperCase method
--- interview-questions.md
+++ interview-questions.md
@@ -10,32 +10,33 @@ Type your initials here to confirm you read the instructions:
 
 1. What is the difference between a parameter and an argument?
 
-Your answer:
+Your answer: parameter is a place holder for a value and an argument is the value. 
 
-Researched answer:
+Researched answer: 
+A parameter is a variable in a function's definition and An argument is the actual value that is passed to the function when it is called.
 
 2. What is the difference between an object and a class?
 
-Your answer:
+Your answer: class is a blueprint to create objects and object is created from class.
 
-Researched answer:
+Researched answer: A class is a blueprint or a template for creating objects and An object is an instance of a class. It's a concrete entity created from the class, which has its own set of values for the attributes defined in the class.
 
 3. What is iteration?
 
-Your answer:
+Your answer: an action that can be use multiple times.
 
-Researched answer:
+Researched answer: performing particular action a certain number of times.
 
 4. STRETCH: What is hoisting in JavaScript?
 
-Researched answer:
+Researched answer: Hoisting is a behavior in JavaScript where variable and function declarations are moved to the top of their containing scope during the compile phase. 
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
-1. Spread operator:
+1. Spread operator: s a powerful feature introduced in ECMAScript 6 (ES6) that allows you to expand elements in places where multiple elements or values are expected.
 
-2. React:
+2. React: is an open-source JavaScript library for building user interfaces, particularly for single-page applications where UI updates are frequent.
 
-3. JSX:
+3. JSX: JSX stands for JavaScript XML. It's a syntax extension for JavaScript that looks similar to XML or HTML, but it allows you to write HTML-like code within JavaScript files.
